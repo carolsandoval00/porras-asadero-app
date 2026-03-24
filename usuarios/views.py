7ff2b811de7d7f54708b969ae425eda17d1c4a7d
fergiee07 <rinconfergie9@gmail.com>
2026-03-24 13:20:27 -0500

    Update views.py

--- usuarios/views.py
+++ usuarios/views.py
@@ -16,16 +16,3 @@ def inicio_usuarios(request):
     usuarios = Usuario.objects.all()
     return render(request, 'inicio_usuarios.html', {'usuarios': usuarios})
 
-
-@login_required
-def actualizar_usuarios(request, id):
-    usuario = get_object_or_404(Usuario, id=id)
-
-    if request.method == "POST":
-        usuario.nombre = request.POST.get("nombre")
-        usuario.email = request.POST.get("email")
-        usuario.telefono = request.POST.get("telefono")
-        usuario.save()
-        return redirect('inicio_usuarios')
-
-    return render(request, 'actualizar_usuarios.html', {'usuario': usuario})
