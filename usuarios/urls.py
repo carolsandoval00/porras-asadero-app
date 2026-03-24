ee67a37a0aa4bbc603fd6223610ba5d12c455117
deisyvalentinamoramorales-maker <deisyvalentinamoramorales@gmail.com>
2026-03-24 13:26:29 -0500

    he actualizado cambios en el codigo en la parte de consultar

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,4 +5,15 @@ from usuarios.views import *
 
 urlpatterns = [
     path('', inicio_usuarios, name='inicio_usuarios'), 
+<<<<<<< Updated upstream
+=======
+]
+
+from django.urls import path
+from .views import DetalleUsuarioView
+
+urlpatterns = [
+    # El <int:pk> es fundamental para saber qué usuario consultar
+    path('consultar/<int:pk>/', DetalleUsuarioView.as_view(), name='detalle_usuario'),
+>>>>>>> Stashed changes
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -6,4 +6,17 @@ def inicio_usuarios(request):
         'nombre' : nombre,
         'titulo' : 'Usuarios',
     }
+<<<<<<< Updated upstream
     return render(request, 'inicio_usuarios.html', context) 
+=======
+    return render(request, 'inicio_usuarios.html', context) 
+
+from django.views.generic import DetailView
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+def DetalleUsuarioView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'usuarios/detalle_usuario.html'
+    context_object_name = 'perfil' 
+>>>>>>> Stashed changes
