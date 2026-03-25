fdb26c0c208bd6e72980802aa2fecdaed8c142d9
carolsandoval00 <carol.sandoval025@gmail.com>
2026-03-25 13:19:08 -0500

    feat: recuperación de contraseña corregida

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from usuarios.views import *
 
 urlpatterns = [
-    path('', inicio_usuarios, name='inicio_usuarios'),
-    path('recuperar/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('recuperar/enviado/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/completo/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'), 
+    path('recuperar/', auth_views.PasswordResetView.as_view(
+        template_name='usuarios/recuperar.html'  # 👈 tu template
+    ), name='recuperar'),
 ]
