1545143c3a508935fbe3bb8afd9d3a2315d3b0de
AntonyBotelloH <antonybotello@gmail.com>
2026-04-09 17:15:52 -0500

    fix: ajustar modelo de usuario

--- core/settings.py
+++ core/settings.py
@@ -101,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTH_USER_MODEL = 'usuarios.Usuario'
+
 
 
 # Internationalization
