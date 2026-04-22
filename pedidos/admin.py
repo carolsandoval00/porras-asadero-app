924bd4fe38ad2ac08308b19aa45fd9d39b94fb22
carolsandoval00 <carol.sandoval025@gmail.com>
2026-04-22 12:47:21 -0500

    reemplazar registros simples por clases ModelAdmin personalizadas

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Pedido)
-admin.site.register(Producto)
-admin.site.register(Orden)
+from .models import Pedido, Orden, Pago, Caja
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display  = ['pk', 'cliente', 'estado', 'total', 'fecha_creacion']
+    list_filter   = ['estado']
+    search_fields = ['cliente']
+
+@admin.register(Orden)
+class OrdenAdmin(admin.ModelAdmin):
+    list_display = ['numero_orden', 'pedido', 'estado', 'total', 'creada_en']
+    list_filter  = ['estado']
+
+@admin.register(Pago)
+class PagoAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'orden', 'metodo', 'monto', 'estado', 'fecha_pago']
+    list_filter  = ['estado', 'metodo']
+
+@admin.register(Caja)
+class CajaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'responsable', 'saldo_inicial', 'estado', 'fecha_apertura']
+    list_filter  = ['estado']
