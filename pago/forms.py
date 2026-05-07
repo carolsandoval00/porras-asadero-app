7cd13d317dff2daf4822940385be1c825b59493d
deisyvalentinamoramorales-maker <deisyvalentinamoramorales@gmail.com>
2026-05-07 14:13:15 -0500

    feat: se conecto la orden con pedidos

--- pago/forms.py
+++ pago/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from .models import Pago
-
+from pedidos.models import Orden
 
 class PagoForm(forms.ModelForm):
     class Meta:
         model  = Pago
-        fields = ['metodo_pago', 'monto', 'referencia', 'estado', 'descripcion']
+        fields = ['orden', 'metodo_pago', 'monto', 'referencia', 'estado', 'descripcion']
         widgets = {
+            'orden':       forms.Select(attrs={'class': 'form-select'}),
             'metodo_pago': forms.Select(attrs={'class': 'form-select'}),
             'monto':       forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'placeholder': '0.00'}),
             'referencia':  forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nro. de referencia (opcional)'}),
